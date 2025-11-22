965b8203447a8e460b278e6149eb81e43672c78c
Niti0225 <niti_02@outlook.de>
2025-11-22 15:54:35 +0100

    Delete metro.config.js

--- metro.config.js
+++
@@ -1,7 +0,0 @@
-// metro.config.js
-const { getDefaultConfig } = require('@expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// ✨ Nichts Spezielles hier – keine createModuleIdFactory, keine extra Transformer.
-module.exports = config;
