7d2866730f8dee486ae9e957339b98170d9abdec
Niti0225 <niti_02@outlook.de>
2025-11-22 15:54:15 +0100

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-import 'react-native-gesture-handler'; // muss erste Zeile sein
-import 'react-native-reanimated';       // Reanimated Runtime laden
-
-import { registerRootComponent } from 'expo';
-import App from './App';
-
-registerRootComponent(App);
