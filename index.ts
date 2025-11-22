65841a12e75cfe99e785aa48c0cceb09af343659
Niti0225 <niti_02@outlook.de>
2025-11-22 15:54:28 +0100

    Delete index.ts

--- index.ts
+++
@@ -1,6 +0,0 @@
-// index.js (Projekt-Root)
-import { registerRootComponent } from 'expo';
-import App from './App';
-
-registerRootComponent(App);
-// -> Lädt App.tsx / App.js automatisch in Expo Go und im Dev Client
